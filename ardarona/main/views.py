7b43b8896bd27a3719e242da0f465a47392d9a8d
Oleja <gghgghyn@gmail.com>
2021-11-27 15:40:23 +0300

    1

--- ardarona/main/views.py
+++ ardarona/main/views.py
@@ -4,18 +4,9 @@ from django.views.generic import DetailView, ListView
 from .models import *
 
 
-
 def index(request):
+    home = Home.objects.all()
     team = Team.objects.all()
     pril = PrilTeam.objects.all()
-    context ={
-        'team':team,
-        'pril':pril
-    }
-    return render(request, 'main/index.html', context=context)
-
-
-
-
-
-
+    context = {"team": team, "pril": pril, "home": home}
+    return render(request, "main/index.html", context=context)
